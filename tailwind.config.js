6516713ef2014e3178937ff17cac5f5e4ead29a7
faresta99 <hendrayulius090@gmail.com>
2025-01-06 23:38:27 +0800

    Deskripsi perubahan yang dilakukan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("rippleui")],
-};
-module.exports = {
-  // ... the rest of your config
   plugins: [require("rippleui")],
 };
